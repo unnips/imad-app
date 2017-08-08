8d4d9bb43036453e60824b2561fcb54a3f76ccdc
unnips <30435399+unnips@users.noreply.github.com>
2017-08-08 21:18:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('Unique-ID');
 
 element.innerHTML = 'New Value';
+
+var img =  document.getElementById('madi');
+
+img.onclick = function () {
+    img.style.marginleft='100px';
+};
