73868f5658d2d4db5bcec214fbad62afd1642f8c
unnips <30435399+unnips@users.noreply.github.com>
2017-08-08 20:20:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ function createTemplate (data){
 }
 
 app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
