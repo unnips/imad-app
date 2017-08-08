d893cd524d22fcba0deea24f11f477efcb8334c6
unnips <30435399+unnips@users.noreply.github.com>
2017-08-08 13:15:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one', function (req, res) {
+  res.send('Article One Requested');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article Two Requested');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article Three Requested');;
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
