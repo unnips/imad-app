32f4650cb8618f04eb5fff3be6f0287443fd8cbf
unnips <30435399+unnips@users.noreply.github.com>
2017-08-08 22:57:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ var element = document.getElementById('Unique-ID');
 element.innerHTML = 'New Value';
 
 var img =  document.getElementById('madi');
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
