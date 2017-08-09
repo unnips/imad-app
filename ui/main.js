13fc1e647870c2899bc9ade88a7598e6ecee2992
unnips <30435399+unnips@users.noreply.github.com>
2017-08-09 22:25:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 // Counter code
 var button = document.getElementById('ctrbtn');
-console.log('Inside main');
+
 button.onclick = function () {
     // Creae a request object
     var request = new XMLHttpRequest();
@@ -8,7 +8,7 @@ button.onclick = function () {
     // Capture the response and store it in a variable
     request.onreadystatechange = function (){
          console.log('Inside function');
-        if(request.readystat === XMLHttpRequest.DONE) {
+        if(request.readystate === XMLHttpRequest.DONE) {
             //Take action
                     console.log('Inside if');
             if (request.status === 200) {
