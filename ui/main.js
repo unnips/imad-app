d52b25e1538f1209eadeb624e06ae3673c4cbec0
unnips <30435399+unnips@users.noreply.github.com>
2017-08-09 21:39:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,14 @@
-console.log('Loaded!');
+// Counter code
+var button = document.getElementById('ctrbtn');
+var counter = 0;
 
-var element = document.getElementById('Unique-ID');
-
-element.innerHTML = 'New Value';
-
-var img =  document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-    if (marginLeft == 100)
-    {
-       return;
-    }
+button.onclick = function () {
+    // Make a request to the counter Endpoint
+    
+    // Capture the response and store it in a variable
+    
+    // Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};
