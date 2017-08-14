a10d8574db23a4ffe523f9c84604f58a3bc8cfee
unnips <30435399+unnips@users.noreply.github.com>
2017-08-14 20:41:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/test-db', function(req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
     
-    pool.query("Select * from article where title = " + req.params.articleName, function(err, result){
+    pool.query("Select * from article where title = '" + req.params.articleName + "'", function(err, result){
         if (err){
             res.status(500).send(err.toString());
         } else {
