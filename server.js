f52600d3942a84abe7707eb711f6485448e79889
unnips <30435399+unnips@users.noreply.github.com>
2017-08-14 20:31:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,9 +123,23 @@ app.get('/test-db', function(req, res) {
     });
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
+    
+    Pool.Query("Select * from article where title = " + req.params.articleName, function(err, result){
+        if (err){
+            res.status(500).send(toString());
+        } else {
+            if (result.rows.length == 0) {
+                res.status(400).send('Article Not Found')
+            }
+            else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    })
     var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  
 });
 
 app.get('/', function (req, res) {
