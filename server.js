850f499915a571eb8eb2dce4b9d5975180cb6278
unnips <30435399+unnips@users.noreply.github.com>
2017-08-21 13:31:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'unni6e',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
                 
 function createTemplate (data){
     var title = data.title;
@@ -61,7 +63,20 @@ function hash(input, salt) {
    return ['pbkdf2', '10000', salt, hashed.toString('hex')].join('$') ;
 }
 
-
+app.post('/create-user', function (req, res){
+   
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" VALUES ($1, $2)', [username, dbString], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        res.send('User Successfully Created' + username);
+    }
+   });
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
