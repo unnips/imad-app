c26b9a9833645c133f7e14937c4a6ba2a05b9b64
unnips <30435399+unnips@users.noreply.github.com>
2017-08-21 12:35:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/hash/:input', function(req, res) {
 
 function hash(input, salt) {
    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-   return [pbkdf2, '10000', salt, hashed.toString('hex')].join('$') ;
+   return ['pbkdf2', '10000', salt, hashed.toString('hex')].join('$') ;
 }
 
 
