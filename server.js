2e80aa77c7a37567c30bc6e42d79c4418f94da95
unnips <30435399+unnips@users.noreply.github.com>
2017-08-21 13:54:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
 
-   pool.query('SELECT * FROM "user" username=$1', [username], function (err, result) {
+   pool.query('SELECT * FROM "user" where username=$1', [username], function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
     } else {
