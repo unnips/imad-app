e94547b9f11f2f0239185c64441abf9f54b7dd48
unnips <30435399+unnips@users.noreply.github.com>
2017-08-21 19:22:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,4 @@
-// Counter code
-var button = document.getElementById('ctrbtn');
-
-button.onclick = function () {
-    // Creae a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function (){
-         console.log('Inside function');
-        if(request.readyState === XMLHttpRequest.DONE) {
-            //Take action
-                    console.log('Inside if');
-            if (request.status === 200) {
-                        console.log('Inside inner loop');
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                console.log('Done');
-            }
-        }
-    };
-    // Render the variable in the correct span
-    request.open('GET', 'http://unni6e.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-var nameInput = document.getElementById('name');
-var submit = document.getElementById('submit_btn');
-
+// Submit username password to login
 submit.onclick = function () {
     // Make a request to the server and send the name
     var request = new XMLHttpRequest();
@@ -37,19 +8,21 @@ submit.onclick = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             //Take action
             if (request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-        
-            var list = '';
-            for (var i=0; i < names.length; i++)
-                list += "<li>"+names[i]+'</li>';
+                console.log('User logged in');
+                alert('Logged in Successfully');
+            } else if (request.status === 403) {
+                alert ('Username/Password is incorrect');
+            } else if (request.statuts === 500) {
+                alert('Something went wrong on the server');
             }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
         }
     };
-    var name = nameInput.value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     // Render the variable in the correct span
-    request.open('GET', 'http://unni6e.imad.hasura-app.io/submit-names?name='+name, true);
-    request.send(null);
+    request.open('POST', 'http://unni6e.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
