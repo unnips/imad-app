ec9782ab228eae4e70c76eb4f0a5339d3d2a9cd2
unnips <30435399+unnips@users.noreply.github.com>
2017-08-21 13:53:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,33 @@ app.post('/create-user', function (req, res){
    });
 });
 
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+
+   pool.query('SELECT * FROM "user" username=$1', [username], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(403).send('username/password invalid')
+        } else {
+        // Match the password
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+            var hashedPassword = hash(password, salt);
+            
+            if(hashedPassword === dbString) {
+                res.send('User Successfully logged in');
+            } else {
+                res.status(403).send('username/password invalid')
+            }
+        }
+    }
+   });
+});
+
+
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
