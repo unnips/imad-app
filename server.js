cbdd4a1d8b2a239c7f4696dfd48145d578d0e8bb
unnips <30435399+unnips@users.noreply.github.com>
2017-08-22 12:51:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/login', function (req, res) {
             var hashedPassword = hash(password, salt);
             
             if(hashedPassword === dbString) {
-                req.session.auth = result.rows[0].id;
+                req.session.auth = {userId: result.rows[0].id};
                 res.send('User Successfully logged in');
             } else {
                 res.status(403).send('username/password invalid');
